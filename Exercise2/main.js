8b7546571b5becf856d8474260c0506009fcf21d
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-17 11:46:46 +0300

    Exercise 2 Waan xaliyay

---
+++ Exercise2/main.js
@@ -0,0 +1,41 @@
+let a = 15;
+let b = "15";
+
+// Equal to (==)
+// Waxay eegaysaa value-ga oo keliya.
+console.log(a == b); // true
+
+
+// Not equal to (!=)
+// Waxay eegaysaa in values-ku kala duwan yihiin.
+console.log(a != b); // false
+
+
+// Strict equal to (===)
+// Waxay eegaysaa value + data type.
+console.log(a === b); // false
+
+
+// Strict not equal to (!==)
+// Waxay eegaysaa value ama data type inuu kala duwan yahay.
+console.log(a !== b); // true
+
+
+// Greater than (>)
+// Waxay eegaysaa x inuu ka weyn yahay y.
+console.log(a > 5); // true
+
+
+// Less than (<)
+// Waxay eegaysaa x inuu ka yar yahay y.
+console.log(a < 5); // false
+
+
+// Greater than or equal to (>=)
+// Waxay eegaysaa x inuu ka weyn yahay ama la mid yahay 10.
+console.log(a >= 10); // true
+
+
+// Less than or equal to (<=)
+// Waxay eegaysaa x inuu ka yar yahay ama la mid yahay 10.
+console.log(a <= 10); // true
