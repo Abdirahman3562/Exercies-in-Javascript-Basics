3cbe06dbd9d8d0de77641a3631ca7829ee462916
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-17 10:34:07 +0300

    Fix Exercise1 folder

---
+++ Exercise1/main.js
@@ -0,0 +1,35 @@
+let x= 7;
+let y =5;
+
+console.log(x);
+console.log(y);
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+console.log(++x);
+console.log("pre increment");
+console.log(x);
+
+// postincrement
+console.log(x++);
+console.log("post increment");
+console.log(x);
+
+
+
+// preDecrement
+// console.log(--x);
+console.log("pre Decrement");
+console.log(x);
+
+// postincrement
+console.log(x--);
+console.log("post Decrement");
+console.log(x);
+
+
+
