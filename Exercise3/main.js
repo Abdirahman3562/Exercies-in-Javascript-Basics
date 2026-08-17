b1def8c3d0b40eb7c9a9581c6d6bda0ece77a087
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-17 12:13:29 +0300

    Exercise 3 Waan xaliyay

---
+++ Exercise3/main.js
@@ -0,0 +1,22 @@
+
+let isSunny = true;
+let isWeekend = true;
+
+// and &&
+// Labada dhinacba waa inay true noqdaan si uu true u soo baxo
+console.log(isSunny && isWeekend); 
+console.log("xeebta waa aadi kartaa")
+// Output: false
+
+// or ||
+// Ugu yaraan hal dhinac haduu true yahay wuxuu soo celinayaa true
+console.log(isSunny || isWeekend); 
+console.log("xeebta waa aadi kartaa");
+// Output: true
+
+// not !
+// Wuxuu rogaa qiimaha (true -> false, false -> true)
+console.log(!isSunny); // Output: false
+console.log("xeebta ma aadi kartid");
+console.log(!isWeekend); // Output: false
+console.log("xeebta ma aadi kartid");
